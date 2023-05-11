207a91beff6cc31f09fbc77def475a362c1bd810
Delos-343 <mfachry.handoko@gmail.com>
2023-05-11 13:58:43 +0700

    changed theme color / bg

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       home: Scaffold(),
     );
