9dba9be25c93435429481a95506572b7823e7410
Delos-343 <mfachry.handoko@gmail.com>
2023-05-11 16:54:21 +0700

    added new Widget for neu_circle.dart

--- lib/neu_circle.dart
+++ lib/neu_circle.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class NeuCircle extends StatelessWidget {
   final child;
   const NeuCircle({
-      super.key,
-      required this.child
-    });
+    super.key,
+    required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
