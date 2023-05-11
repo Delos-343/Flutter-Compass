8bbf1e8633d2e0e2bcd0877ff2ceb4412d30adfb
Delos-343 <mfachry.handoko@gmail.com>
2023-05-11 15:17:51 +0700

    added orange bg to main Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.orange,
       ),
       home: Scaffold(
+        backgroundColor: Colors.orange,
         body: Builder(builder: (context) {
           if(_hasPermissions) {
             return _buildCompass();
