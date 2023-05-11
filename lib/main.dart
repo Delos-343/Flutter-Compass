1623c452fba25f1b1ba524fa7efd7485b2476c52
Delos-343 <mfachry.handoko@gmail.com>
2023-05-11 13:57:55 +0700

    stressed out

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Compass App | Fachry',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         primarySwatch: Colors.blue,
